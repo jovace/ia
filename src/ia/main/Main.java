0a266b0e3fee02df9c7acd699c80c9957ad7a413
jvazquac <jvazquac@MAD-9WF9NH2.usersad.everis.int>
2018-11-26 15:06:51 +0100

    Prueba inicial parser

--- src/ia/main/Main.java
+++ src/ia/main/Main.java
@@ -4,13 +4,18 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import ia.models.Grafo;
+import ia.models.Vertice;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Grafo mapaMetro = new Grafo();
+		
+		Map<String,Vertice> estaciones = new HashMap<>();
 		
 		
 		try {
@@ -20,9 +25,42 @@ public class Main {
 
 			BufferedReader br = new BufferedReader(fr);
 			while ((cadena = br.readLine()) != null) {
-				System.out.println(cadena);
+				//Nombre;[Ady1:dist1;ady2:dist2]
+				Vertice vert;
+				String nombre = cadena.split(";")[0];
+				
+				if(!estaciones.containsKey(nombre)) {
+					vert=new Vertice(nombre);
+					estaciones.put(nombre, vert);
+					mapaMetro.addVertice(vert);
+				}else {
+					vert=estaciones.get(nombre);
+				}
+			
+				
+				String conexiones=cadena.split(";")[1].replace('[', ' ').replace(']',' ').trim();
+				String[] adyacentes = conexiones.split(";");
+				for(String ady : adyacentes) {
+					String estacion=ady.split(":")[0];
+					Double peso=Double.valueOf(ady.split(":")[1]);
+					
+					Vertice est;
+					if(!estaciones.containsKey(estacion)) {
+						est=new Vertice(estacion);
+						estaciones.put(estacion, est);
+						mapaMetro.addVertice(est);
+					}else {
+						est=estaciones.get(estacion);
+					}
+					
+					if(!mapaMetro.conectarVertices(vert, est, peso)) {
+						//Error
+					}
+				}
 			}
 			br.close();
+			
+			System.out.println("Importacion correcta");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
